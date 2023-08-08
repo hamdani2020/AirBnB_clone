b28f81407985602b5e370440a87c225fae40b087
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-08-08 15:13:09 -0700

    updated models directory

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Amenity Module"""
+
+from models.base_model import BaseModel
+"""Import base model"""
+
+class Amenity(BaseModel):
+    """Instantiation of Amenity class"""
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""City Module"""
+
+from models.base_model import BaseModel
+
+class City(Basemodel):
+    """Instantiation city class"""
+    state_id = ""
+    name = ""
