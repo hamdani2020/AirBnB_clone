96f9eb99d588e494bb09318cee59d6490736ce22
eric_boateng <boateng.eric44@gmail.com>
2023-08-11 13:33:20 +0000

    fixed bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
@@ -18,21 +17,10 @@ class FileStorage:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
-=======
-"""File storage module"""
-import datetime
-import json
-import os
-
-
-class FileStorage:
-    """Serialization and deserialization of base class"""
->>>>>>> 71b3191da3a8da3c6353147fda3bf69a457358bc
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-<<<<<<< HEAD
         """Return the dictionary __objects."""
         return FileStorage.__objects
 
@@ -59,47 +47,4 @@ class FileStorage:
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
-=======
-        """It returns objects dictionary"""
-        return FileStorage.__objects
 
-    def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
-
-    def save(self):
-        """Serializes __objects to the JSON file"""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            data = {a: b.to_dict() for a, b in FileStorage.__objects.items()}
-            json.dump(data, f)
-    def classes(self):
-        """It returns valid class dictionaries"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "Amenity": Amenity,
-                   "City": City,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
-
-    def reload(self):
-        """deserializes the JSON file to __objects"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {a: self.classes()[b["__class__"]](**b)
-                    for a, b in obj_dict.items()}
-            FileStorage.__objects = obj_dict
-
->>>>>>> 71b3191da3a8da3c6353147fda3bf69a457358bc
